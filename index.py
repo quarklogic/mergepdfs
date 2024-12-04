a19247942134a560d3ea553af5983aeb1dc7a167
Carlos Ramirez <carlos@quarklogic.com>
2024-12-03 21:45:08 -0800

    - Added uuid module to create unique directory names for each request - Added shutil module to remove directory recursively - Updated logging to default to INFO

--- index.py
+++ index.py
@@ -7,15 +7,22 @@ import os
 import logging
 import PyPDF2
 import re
+import uuid
+import shutil
 
 
 app = Flask(__name__)
 
+## Script Configuration
+
 ## Limit uploads to 20 MB
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1000 * 1000
-max_upload = '20 MB'
+MAX_UPLOAD = '20 MB'
+TMP_DIR    = './tmp'
 
-logging.basicConfig(filename='log/app.log')
+## End of Script Configuration
+
+logging.basicConfig(filename='log/app.log', level=logging.INFO)
 
 def process_pdfs():
     logging.info('[process_pdfs()]')
@@ -26,44 +33,51 @@ def process_pdfs():
     
     if pdf_file_count < 2:
         return f"ERROR: You must submit at least two files. Please try again"
-
     
-    pdf_files = []
-    merged_pdf_filename = f"tmp/{request.form['pdf_filename']}" if request.form['pdf_filename'] else 'tmp/merged_files.pdf'
+    ## Determin the name of the merged pdf filename
+    merged_pdf_filename = request.form['pdf_filename'] or 'merged_files.pdf'
     merged_pdf_filename = merged_pdf_filename.replace(' ', '_')
  
-    
     ## Append the .pdf extension if not included in the filename
     if not re.search('.pdf$', merged_pdf_filename):
         merged_pdf_filename = ''.join([merged_pdf_filename, '.pdf'])
         
     logging.info(f'[process_pdfs()] {merged_pdf_filename=}') 
+
+    ## Create the a unique temporary directory for this request        
+    unique_id = uuid.uuid4()
+    tmp_dir = ''.join([TMP_DIR, '/', str(unique_id)])
     
+    if not os.path.exists(tmp_dir):
+        logging.info(f'[process_pdfs()] Creating {tmp_dir=}')
+        os.makedirs(tmp_dir)
+            
     ## Save the uploaded PDF files
+    pdf_files        = []
     bad_pdf_filename = ''
     for file in uploaded_files:
         logging.info(f'[process_pdfs()] Processing uploaded pdf file: {file.filename}')
         
         if not re.search('\.pdf$', file.filename, re.IGNORECASE):
-            bad_pdf_filename = f'Found bad extension on {file.filename}'
+            bad_pdf_filename = f'ERROR: Found bad extension on {file.filename}'
             logging.info(f'[process_pdfs()] {bad_pdf_filename}')
             
             break
         
-        if not re.search('^[\w\s\-\_]+\.pdf$', file.filename, re.IGNORECASE):
-            bad_pdf_filename = f'Found bad characters in filename {file.filename}'
+        if not re.search('^[\w\s\-\_\.]+\.pdf$', file.filename, re.IGNORECASE):
+            bad_pdf_filename = f'ERROR: Found bad characters in filename {file.filename}'
             logging.info(f'[process_pdfs()] {bad_pdf_filename}')
             
             break    
-            
-        local_filename = ''.join(['./tmp/', file.filename])
+                
+        local_filename = ''.join([tmp_dir, '/', file.filename])
         local_filename = local_filename.replace(' ', '_')
         local_filename = local_filename.lower()
         
         logging.info(f'[process_pdfs()] {local_filename}')
                 
         if file.filename != '':
-            tmp_filename = ''.join(['./tmp/', file.filename])
+            tmp_filename = ''.join([tmp_dir, '/', file.filename])
             
             logging.info(f'[process_pdfs()] saving {tmp_filename}')
             file.save(tmp_filename)
@@ -74,9 +88,13 @@ def process_pdfs():
             
     
     if bad_pdf_filename:
-        return f"ERROR: Bad filename: {bad_pdf_filename}"
+        if os.path.exists(tmp_dir):
+            shutil.rmtree(tmp_dir)
+            
+        return bad_pdf_filename
     
     ## Merge the PDF files
+    merged_pdf_filename = "".join([tmp_dir, '/', merged_pdf_filename])
     logging.info(f'[process_pdfs()] merging {pdf_files=}')
     merger = PyPDF2.PdfMerger()
     for pdf_file in pdf_files:
@@ -91,8 +109,6 @@ def process_pdfs():
         for pdf_file in pdf_files:
             os.remove(pdf_file)
             
-                
-    #post_message = f"{pdf_file_count} PDF files merged successfully to {merged_pdf_filename}"
     return merged_pdf_filename
  
 def stream_pdf(merged_pdf_filename):
@@ -121,6 +137,7 @@ def index():
             ## Determine filename from the merged_pdf_filename path
             match = re.search('\/([^/]+)$', merged_pdf_filename)
             merged_filename = match.group(1) if match else 'merged_files.pdf'
+            logging.info(f'Streaming {merged_pdf_filename=} and sending out {merged_filename=}')
             
             response = Response(stream_pdf(merged_pdf_filename), mimetype='application/pdf')
             response.headers["Content-Disposition"] = f"attachment; filename={merged_filename}"
@@ -130,7 +147,7 @@ def index():
                
     return render_template("index.html", 
                            post_message=post_message, 
-                           max_upload=max_upload,
+                           max_uploade=MAX_UPLOAD,
                            app_uri=request.environ.get('SCRIPT_NAME'))
 
 
